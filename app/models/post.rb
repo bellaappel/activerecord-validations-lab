5779a560b675fe88b0db9af392cac1cbee3e1131
bellaappel <bellaappel0@gmail.com>
2020-11-30 15:31:15 -0500

    done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates_with TitleValidator
+
 end
+
+#     CLICKBAIT_PATTERNS = [
+#         "Won't Believe",
+#         "Secret",
+#         "Top [number]",
+#         "Guess"
+#     ]
+
+#     def title_include
+#         if title.present? && CLICKBAIT_PATTERNS.none? { |p| title.match(p) }
+#         errors.add(:title, "title not clickbaity enough")
+#         end
+#     end
+# end
